f127d7407a25a6398a09f643e2327b78f03b5316
Dipendu <97115457+dipmndl@users.noreply.github.com>
2022-06-07 00:02:26 +0530

    completed

---
+++ Assignment_6.py
@@ -0,0 +1,34 @@
+#1.	What are escape characters, and how do you use them?
+''' To insert characters that are illegal in a string, use an escape character. An escape character is a backslash \ followed by the character you want to insert.'''
+#2. What do the escape characters n and t stand for?
+''' New line and Tab'''
+#3. What is the way to include backslash characters in a string?
+    # \\ backslash
+#4. The string "Howl's Moving Castle" is a correct value. Why isn't the single quote character in the word Howl's not escaped a problem?
+''' The single quote in Howl's is fine because you've used double quotes to mark the beginning and end of the string.'''
+#5. How do you write a string of newlines if you don't want to use the n character?
+# 6. What are the values of the given expressions?
+        #'Hello, world!'[1]
+s = "Hello, world!"
+print(s[1])
+        #'Hello, world!'[0:5]
+print(s[0:5])       
+        #'Hello, world!'[:5]
+print(s[:5])   
+        #'Hello, world!'[3:]
+print(s[3:])      
+#7. What are the values of the following expressions?
+        #'Hello'.upper()
+print('Hello'.upper())
+        #'Hello'.upper().isupper()
+print('Hello'.upper().isupper())
+        #'Hello'.upper().lower()
+print('Hello'.upper().lower())
+#8. What are the values of the following expressions?
+    #'Remember, remember, the fifth of July.'.split()
+    #'-'.join('There can only one.'.split())
+print('Remember, remember, the fifth of July.'.split())
+print('-'.join('There can only one.'.split()))
+#9. What are the methods for right-justifying, left-justifying, and centering a string?
+#10. What is the best way to remove whitespace characters from the start or end?
+
